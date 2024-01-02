036436c7435a1e52db1f363bec6dc9b76b4fc6b8
Dogkaebi5 <st9dog@gmail.com>
2024-01-03 00:44:22 +0900

    서브 네비: params(커리) tag 확인, 선택된 버튼 CSS변경

--- src/app/components/SubNav.js
+++ src/app/components/SubNav.js
@@ -1,28 +1,34 @@
 'use client'
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useSearchParams } from "next/navigation"
 
 const SubNav = () => {
   const pathname = usePathname()
+  const params = useSearchParams()
+  const tag = params.get('tag')
   let nav = [];
 
   if (pathname == "/cantonese") nav = ["한자", "음절", "회화", "문화"];
   if (pathname == "/code") nav = ["HTML", "CSS", "JavaScript"];
   if (pathname == "/blog") nav = ["중국 문화", "책", "일상"];
 
+  const activeClass = "rounded-full border py-2 px-8 mr-2 bg-slate-600 text-white"
+  const notActiveClass = "rounded-full border py-2 px-8 mr-2"
+  let isAll = true
 
   return (
     <>
       <div className="flex justify-between items-center mt-10 mb-4">
         <div>
-          <Link href="/cantonese" className="rounded-full border py-2 px-8 mr-2 bg-slate-600 text-white">All</Link>
-          {nav.map((item) => (
-            <Link 
-              href={"/cantonese?tag="+item}
+          <Link href={pathname} 
+            className={tag==null?activeClass:notActiveClass}>All</Link>
+          {nav.map((item) => {
+            return <Link 
+              href={pathname+"?tag="+item}
               key={item}
-              className={'rounded-full border py-2 px-8 mr-2'}>{item}
+              className={tag==item?activeClass:notActiveClass}>{item}
             </Link>
-          ))}
+          })}
         </div>
         <form>
           <input className="border rounded-lg pl-2 py-1 w-40 mr-2"/>
