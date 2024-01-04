a095a2eab526cd208a8ae6f8738a5916aaf6875c
Dogkaebi5 <st9dog@gmail.com>
2024-01-04 13:45:02 +0900

    card 리스트 640px 보다 작을 때 1개 크기로 적용: image-w full, grid-cols-1 = 기본

--- src/app/components/PageCards.js
+++ src/app/components/PageCards.js
@@ -25,33 +25,40 @@ const PageCards = () => {
       "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "id": 1,
-      "title": "제목 샘플 1",
+      "id": 4,
+      "title": "제목 샘플 4",
       "date": "2999.12.31",
       "thumbnail": "https://placehold.co/600x400.png",
       "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "id": 2,
-      "title": "제목 샘플 2",
+      "id": 5,
+      "title": "제목 샘플 6",
       "date": "2999.12.31",
       "thumbnail": "https://placehold.co/600x400.png",
       "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "id": 3,
-      "title": "제목 샘플 3",
+      "id": 6,
+      "title": "제목 샘플 6",
+      "date": "2999.12.31",
+      "thumbnail": "https://placehold.co/600x400.png",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      "id": 7,
+      "title": "제목 샘플 7",
       "date": "2999.12.31",
       "thumbnail": "https://placehold.co/600x400.png",
       "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
   ]
   return ( 
-    <div className="mt-4 grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="mt-4 grid grid-cols grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {data.map((post) => 
-          <div key={post.id} className="mt-4 max-w-96 bg-slate-200">
+          <div key={post.id} className="mt-4 bg-slate-200">
             <Link href={"/read/"+post.id} >
-              <Image width={600} height={400} src={post.thumbnail} alt="sample img"/>
+              <Image className="w-full" width={600} height={400} src={post.thumbnail} alt="sample img"/>
               <div className="px-4 pt-4 pb-8">
                 <h2 className="font-bold text-lg">{post.title}</h2>
                 <p className="text-gray-700 text-sm pb-2">{post.date}</p>
