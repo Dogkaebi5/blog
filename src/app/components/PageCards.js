fff7f33b1672a5f931c9b3e6e550a6ee30cb8684
Dogkaebi5 <st9dog@gmail.com>
2024-01-04 13:06:06 +0900

    cards 정렬 디자인 변경 flex -> grid

--- src/app/components/PageCards.js
+++ src/app/components/PageCards.js
@@ -24,11 +24,32 @@ const PageCards = () => {
       "thumbnail": "https://placehold.co/600x400.png",
       "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
+    {
+      "id": 1,
+      "title": "제목 샘플 1",
+      "date": "2999.12.31",
+      "thumbnail": "https://placehold.co/600x400.png",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      "id": 2,
+      "title": "제목 샘플 2",
+      "date": "2999.12.31",
+      "thumbnail": "https://placehold.co/600x400.png",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      "id": 3,
+      "title": "제목 샘플 3",
+      "date": "2999.12.31",
+      "thumbnail": "https://placehold.co/600x400.png",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
   ]
   return ( 
-    <div className="mt-8 flex gap-4">
+    <div className="mt-4 grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((post) => 
-          <div key={post.id} className="max-w-80 bg-slate-200">
+          <div key={post.id} className="mt-4 max-w-96 bg-slate-200">
             <Link href={"/read/"+post.id} >
               <Image width={600} height={400} src={post.thumbnail} alt="sample img"/>
               <div className="px-4 pt-4 pb-8">
