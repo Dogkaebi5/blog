57f7bd74fe9b3f07a0b84797a3a05d6362b56044
Dogkaebi5 <st9dog@gmail.com>
2024-01-22 14:33:10 +0900

    음절 파일 위치 변경. syllable2를 메인으로 변경. 각 음 링크, 페이지 추가.(운성모 & 음절)

---
+++ src/app/dictionary/initial/[id]/page.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Initial(props) {
+  return <>이니셜 상세내용...</>;
+}
---
+++ src/app/dictionary/yueyin/[id]/page.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function YueYin(props) {
+  return <>음절 상세내용...</>;
+}
