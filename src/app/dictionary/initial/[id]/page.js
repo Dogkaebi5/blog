597d3cdd15f47ceca511a23eb03147e7422982ee
Dogkaebi5 <st9dog@gmail.com>
2024-01-22 15:45:44 +0900

    각 이니셜 페이지 디자인, param에 따라서 음절 리스트 다르게 표기

--- src/app/dictionary/initial/[id]/page.js
+++ src/app/dictionary/initial/[id]/page.js
@@ -1,5 +1,55 @@
-"use client";
+import Link from "next/link";
+import syllable from "../../yueYin";
 
 export default function Initial(props) {
-  return <>이니셜 상세내용...</>;
+  const yueYin = syllable.yueYin;
+  let alpha = props.params.id;
+
+  if (alpha == "-") alpha = "";
+  function checkIsInital(text) {
+    return !(
+      text.startsWith("a") ||
+      text.startsWith("e") ||
+      text.startsWith("i") ||
+      text.startsWith("o") ||
+      text.startsWith("u") ||
+      text.startsWith("y")
+    );
+  }
+
+  let isInitial = checkIsInital(alpha);
+
+  return (
+    <div className="p-8">
+      <h1 className="p-8 bg-slate-100 inline-block rounded-md font-bold text-6xl">
+        {alpha == "" ? "-" : alpha}
+      </h1>
+      <div className="px-2 my-4">
+        <p className="my-4">
+          {isInitial ? "성모(聲母) " : "음모(韻母) "}발음 : [아~]{" "}
+        </p>
+        <p>긴 [아]의 발음이 나는 운모(韻母)인 동시에 음절(音節)일 수도 있다.</p>
+        <p>모든 성모(聲母)와 조합이 가능한 3개의 운모 중 하나이다.</p>
+      </div>
+      <hr />
+      <div className="p-2">
+        <h3 className="font-bold mb-4">음절 조합</h3>
+        <div className="flex flex-wrap">
+          {yueYin.map((alp) => {
+            return (
+              alpha == ""
+                ? !checkIsInital(alp)
+                : isInitial
+                ? alp.startsWith(alpha)
+                : alp.endsWith(alpha)
+            ) ? (
+              <Link href={`/dictionary/yueyin/${alp}`} key={alp}>
+                <span className="p-1 text-blue-600 underline">{alp}</span>
+              </Link>
+            ) : null;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
