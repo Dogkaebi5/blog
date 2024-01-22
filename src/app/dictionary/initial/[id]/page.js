c95d060c359e6a94f1ceb23e5f53a61a4c1c4fae
Dogkaebi5 <st9dog@gmail.com>
2024-01-22 16:40:35 +0900

    checklist 함수 수정 - vowel 부분도 sort

--- src/app/dictionary/initial/[id]/page.js
+++ src/app/dictionary/initial/[id]/page.js
@@ -21,13 +21,70 @@ export default function Initial(props) {
 
   function checkList(text) {
     if (alpha == "") return !checkIsInital(text);
+    if (alpha == "m") {
+      return (
+        text.startsWith(alpha) ||
+        (text.endsWith("m") &&
+          !text.endsWith("am") &&
+          !text.endsWith("em") &&
+          !text.endsWith("im"))
+      );
+    }
+    if (alpha == "ng") {
+      return (
+        text.startsWith(alpha) ||
+        (text.endsWith("ng") &&
+          !text.endsWith("ang") &&
+          !text.endsWith("eng") &&
+          !text.endsWith("ing") &&
+          !text.endsWith("ong") &&
+          !text.endsWith("ung"))
+      );
+    }
     if (isInitial) {
-      if (alpha == "n") return text.startsWith("n") && !text.startsWith("ng");
-      if (alpha == "g") return text.startsWith("g") && !text.startsWith("gw");
-      if (alpha == "k") return text.startsWith("k") && !text.startsWith("kw");
+      if (alpha == "n") return text.startsWith(alpha) && !text.startsWith("ng");
+      if (alpha == "g") return text.startsWith(alpha) && !text.startsWith("gw");
+      if (alpha == "k") return text.startsWith(alpha) && !text.startsWith("kw");
       return text.startsWith(alpha);
     }
-    return text.endsWith(alpha);
+    if (!isInitial) {
+      if (alpha == "ai") return text.endsWith(alpha) && !text.endsWith("aai");
+      if (alpha == "au") return text.endsWith(alpha) && !text.endsWith("aau");
+      if (alpha == "am") return text.endsWith(alpha) && !text.endsWith("aam");
+      if (alpha == "an") return text.endsWith(alpha) && !text.endsWith("aan");
+      if (alpha == "ang") return text.endsWith(alpha) && !text.endsWith("aang");
+      if (alpha == "ap") return text.endsWith(alpha) && !text.endsWith("aap");
+      if (alpha == "at") return text.endsWith(alpha) && !text.endsWith("aat");
+      if (alpha == "ak") return text.endsWith(alpha) && !text.endsWith("aak");
+      if (alpha == "e") return text.endsWith(alpha) && !text.endsWith("oe");
+      if (alpha == "eng") return text.endsWith(alpha) && !text.endsWith("oeng");
+      if (alpha == "ek") return text.endsWith(alpha) && !text.endsWith("oek");
+      if (alpha == "i") {
+        return (
+          text.endsWith("i") &&
+          !text.endsWith("ai") &&
+          !text.endsWith("ei") &&
+          !text.endsWith("oi") &&
+          !text.endsWith("ui")
+        );
+      }
+      if (alpha == "oi") return text.endsWith(alpha) && !text.endsWith("eoi");
+      if (alpha == "on") return text.endsWith(alpha) && !text.endsWith("eon");
+      if (alpha == "ot") return text.endsWith(alpha) && !text.endsWith("eot");
+      if (alpha == "u") {
+        return (
+          text.endsWith(alpha) &&
+          !text.endsWith("au") &&
+          !text.endsWith("eu") &&
+          !text.endsWith("iu") &&
+          !text.endsWith("ou") &&
+          !text.endsWith("yu")
+        );
+      }
+      if (alpha == "un") return text.endsWith(alpha) && !text.endsWith("yun");
+      if (alpha == "ut") return text.endsWith(alpha) && !text.endsWith("yut");
+      return text.endsWith(alpha);
+    }
   }
 
   return (
