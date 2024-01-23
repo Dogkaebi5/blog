7d046a8ce3309a8bb93a555ab302067569d626a3
Dogkaebi5 <st9dog@gmail.com>
2024-01-23 16:53:46 +0900

    월음: 디자인, 성모운모 구분, 성조 표기, 발음 오디오 링크. 파일 데이터 성조 내용 추가

--- src/app/dictionary/yueyin/[id]/page.js
+++ src/app/dictionary/yueyin/[id]/page.js
@@ -1,5 +1,93 @@
 "use client";
+import syllable from "../../yueYin";
 
 export default function YueYin(props) {
-  return <>음절 상세내용...</>;
+  const yueYin = props.params.id;
+
+  function checkAlpha() {
+    if (
+      yueYin.startsWith("a") ||
+      yueYin.startsWith("e") ||
+      yueYin.startsWith("o") ||
+      yueYin.startsWith("u")
+    )
+      return ["", yueYin];
+
+    if (yueYin.startsWith("gw") || yueYin.startsWith("kw"))
+      return [yueYin.substring(0, 2), yueYin.substring(2)];
+
+    if (yueYin.startsWith("ng")) {
+      if (yueYin.length == 2) {
+        return ["", yueYin];
+      } else {
+        return [yueYin.substring(0, 2), yueYin.substring(2)];
+      }
+    }
+
+    if (yueYin.startsWith("m")) {
+      if (yueYin.length == 1) {
+        return ["", yueYin];
+      } else {
+        return [yueYin.substring(0, 1), yueYin.substring(1)];
+      }
+    }
+
+    return [yueYin.substring(0, 1), yueYin.substring(1)];
+  }
+
+  function checkToens() {
+    const data = syllable.tone;
+    const keys = Object.keys(data);
+
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i] == yueYin) {
+        return data[keys[i]];
+      }
+    }
+  }
+
+  let initial = checkAlpha()[0];
+  let vowel = checkAlpha()[1];
+  let tones = checkToens();
+
+  const audioUrlOrigin =
+    "https://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/sound/";
+
+  const handleAudio = (syllabel) => {
+    const audioUrl = `${audioUrlOrigin}/${syllabel}.Mp3`;
+    const audio = new Audio(audioUrl);
+    audio.play();
+  };
+
+  return (
+    <div className="mt-6 p-4">
+      <div className="flex">
+        <h1 className="p-8 bg-slate-100 inline-block rounded-md font-bold text-6xl">
+          {yueYin}
+        </h1>
+        <div className="p-2">
+          <p>월음 (粵音)</p>
+          <p>발음 : [바~]</p>
+        </div>
+      </div>
+      <div className="my-4 p-2">
+        <p>성모 : {initial}</p>
+        <p>운모 : {vowel}</p>
+      </div>
+      <hr className="mb-4" />
+      {tones.map((tone) => {
+        return (
+          <p
+            key={yueYin + tone}
+            className="p-2 m-2 hover:bg-slate-50"
+            onClick={() => {
+              handleAudio(yueYin + tone);
+            }}
+          >
+            <span className="text-blue-500 underline">{yueYin + tone}</span> 🔊
+          </p>
+        );
+      })}
+    </div>
+  );
 }
