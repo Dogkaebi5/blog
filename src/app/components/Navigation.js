a91eef54d0475aa941769844dfddeb209b8dae0b
Dogkaebi5 <st9dog@gmail.com>
2024-02-14 13:22:51 +0900

    nav 반응형 버거버튼 추가

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -12,12 +13,25 @@ const Navigation = () => {
     ["About", "/about"],
   ];
 
+  const [burgerBtnClass, setBurgerBtnClass] = useState("navbar-burger");
+  const burgerLineClass = "absolute h-1 w-6 rounded-full my-1 bg-slate-800";
+
+  function burgerBtnHandle(e) {
+    e.preventDefault();
+    if (burgerBtnClass == "navbar-burger") {
+      setBurgerBtnClass("navbar-burger-active");
+    } else {
+      setBurgerBtnClass("navbar-burger");
+    }
+    console.log(burgerBtnClass);
+  }
+
   return (
-    <div className="sm:flex justify-between items-center hidden">
-      <Link href={"/"} className="px-4 py-2 bg-slate-100 m-1">
-        logo
+    <div className="flex justify-between items-center my-2">
+      <Link href={"/"} className="py-2 px-4 rounded-full bg-slate-100">
+        DogKaeBi
       </Link>
-      <nav className="flex sm:justify-center space-x-2 my-2">
+      <nav className="navbar-main space-x-2 my-2">
         {navigation.map(([title, url]) => {
           const isActive = pathname === url;
           return (
@@ -35,6 +49,14 @@ const Navigation = () => {
           );
         })}
       </nav>
+      <div
+        className={`${burgerBtnClass} h-7 px-4 cursor-pointer relative mr-4`}
+        onClick={burgerBtnHandle}
+      >
+        <span className={burgerLineClass + " top-0"}></span>
+        <span className={burgerLineClass + " top-2"}></span>
+        <span className={burgerLineClass + " bottom-0"}></span>
+      </div>
     </div>
   );
 };
