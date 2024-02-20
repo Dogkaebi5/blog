3b00284d98879966f87aae49b908d712ba9e5dcd
Dogkaebi5 <st9dog@gmail.com>
2024-02-20 12:01:12 +0900

    pageNav 컴포러트 del. ctrl-setCardList 파라미터 category, pageNum으로 변경(blogCardList에서 category 판단 및 전달)

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Heros from "./components/Heros";
 import SubNav from "./components/SubNav";
 import BlogCardList from "./components/BlogCardList";
-import PageNav from "./components/PageNav";
 
 export default function Home() {
   return (
