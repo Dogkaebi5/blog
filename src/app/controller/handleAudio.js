b64afd70028847cd3dbb4d281cd10a8e2c803cc3
Dogkaebi5 <st9dog@gmail.com>
2024-03-07 12:11:28 +0900

    ctrl handleAudio 삭제, YueYinPlayer 컴포넌트에 함수 포함

--- src/app/controller/handleAudio.js
+++
@@ -1,9 +0,0 @@
-function handleAudio(syllabel) {
-  const audioUrlOrigin =
-    "https://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/sound/";
-  const audioUrl = `${audioUrlOrigin}/${syllabel}.Mp3`;
-  const audio = new Audio(audioUrl);
-  audio.play();
-}
-
-export { handleAudio };
