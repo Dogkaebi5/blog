c32e9e0ad8c582b7278c87e4d8471843fbf00363
Dogkaebi5 <st9dog@gmail.com>
2024-04-19 16:32:11 +0900

    search: 디자인 변경. 검색 결과 없음 틀(텍스트) 추가

--- src/app/(route)/search/page.js
+++ src/app/(route)/search/page.js
@@ -12,6 +12,7 @@ export default function Search(props) {
   const resSy = res?.syllable;
   const resWd = res?.words;
   const resPs = res?.posts;
+  const isNoData = resTc == null && resSy == null && resWd == null && resPs == null;
 
   return (
     <div className={ccss.noHeroContent}>
@@ -19,53 +20,59 @@ export default function Search(props) {
         <p className="mb-8">
           검색어 : <span className=" text-green-500">{searchText}</span>
         </p>
-        {/* 한자 검색결과 */}
-        {resTc != null ? (
-          <div>
-            <p>한자</p>
-            {resTc.map((data) => {
-              return <TcResults key={"tc" + data.sortId} data={data} />;
-            })}
-            <hr />
-          </div>
-        ) : null}
+        {isNoData ? (
+          "결색된 내용이 없습니다"
+        ) : (
+          <>
+            {/* 한자 검색결과 */}
+            {resTc != null ? (
+              <div>
+                <p>한자</p>
+                {resTc.map((data) => (
+                  <TcResults key={"tc" + data.sortId} data={data} />
+                ))}
+                {resSy == null && resWd == null && resPs == null ? null : <hr />}
+              </div>
+            ) : null}
 
-        {/* 발음 검색결과 */}
-        {resSy != null ? (
-          <div className="mt-4">
-            <p>월음</p>
-            <Link className={ccss.linkGreenText + " p-4 text-xl"} href={syllableURL + resSy}>
-              {resSy}
-            </Link>
-            {syllable.yueYin[resSy].tone.map((tone) => (
-              <span className="text-sm mx-2" key={resSy + tone}>
-                {resSy + tone}
-              </span>
-            ))}
-            <hr className="mt-4" />
-          </div>
-        ) : null}
+            {/* 발음 검색결과 */}
+            {resSy != null ? (
+              <div className="mt-4">
+                <p>월음</p>
+                <Link className={ccss.linkGreenText + " p-4 text-xl"} href={syllableURL + resSy}>
+                  {resSy}
+                </Link>
+                {syllable.yueYin[resSy].tone.map((tone) => (
+                  <span className="text-sm mx-2" key={resSy + tone}>
+                    {resSy + tone}
+                  </span>
+                ))}
+                {resWd == null && resPs == null ? null : <hr className="mt-4" />}
+              </div>
+            ) : null}
 
-        {/* 단어 검색결과 */}
-        {resWd != null ? (
-          <div className="mt-4">
-            <p>단어</p>
-            {resWd.map((data) => {
-              return <TcResults key={"wd" + data.sortId} data={data} />;
-            })}
-            <hr />
-          </div>
-        ) : null}
+            {/* 단어 검색결과 */}
+            {resWd != null ? (
+              <div className="mt-4">
+                <p>단어</p>
+                {resWd.map((data) => (
+                  <TcResults key={"wd" + data.sortId} data={data} />
+                ))}
+                {resPs == null ? null : <hr />}
+              </div>
+            ) : null}
 
-        {/* 일기 검색결과 */}
-        {resPs != null ? (
-          <div>
-            <p>관련 포스트</p>
-            {resPs.map((data) => {
-              return <PostResults key={data.slug} data={data} />;
-            })}
-          </div>
-        ) : null}
+            {/* 일기 검색결과 */}
+            {resPs != null ? (
+              <div>
+                <p>관련 포스트</p>
+                {resPs.map((data) => (
+                  <PostResults key={data.slug} data={data} />
+                ))}
+              </div>
+            ) : null}
+          </>
+        )}
       </div>
     </div>
   );
