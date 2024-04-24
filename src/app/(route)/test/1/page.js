0b3541ddf69ab9ddf60b783d4ff9b7e6e3262bf9
Dogkaebi5 <st9dog@gmail.com>
2024-04-24 15:27:28 +0900

    모든 test 주석처리

--- src/app/(route)/test/1/page.js
+++ src/app/(route)/test/1/page.js
@@ -1,58 +1,58 @@
-import { imgURL } from "@controller/urls";
-import * as ccss from "@controller/cssName";
-import Image from "next/image";
-import { B, BQ, CI, CP, FN, FNR, H, I, IMG, LI, LT, M, T, U, X } from "@/app/(route)/test/BlogTextStyle";
+// import { imgURL } from "@controller/urls";
+// import * as ccss from "@controller/cssName";
+// import Image from "next/image";
+// import { B, BQ, CI, CP, FN, FNR, H, I, IMG, LI, LT, M, T, U, X } from "@/app/(route)/test/BlogTextStyle";
 
-export default function Test() {
-  const preCode = `const a = "This is indented code.";
-console.log(a);
-`;
-  const imageIDs = ["1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF", "16TOHBZwX30pmS993qvRbZt8uUXEAR_mm"];
-  const quote = `This is blockquotes
-This is blockquotes line 2`;
+// export default function Test() {
+//   const preCode = `const a = "This is indented code.";
+// console.log(a);
+// `;
+//   const imageIDs = ["1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF", "16TOHBZwX30pmS993qvRbZt8uUXEAR_mm"];
+//   const quote = `This is blockquotes
+// This is blockquotes line 2`;
 
-  const inlineCode = `console.log("hello world")`;
-  const footnotes = ["This is footnote 1.", "This is footnote 2.", "This is footnote 3."];
+//   const inlineCode = `console.log("hello world")`;
+//   const footnotes = ["This is footnote 1.", "This is footnote 2.", "This is footnote 3."];
 
-  return (
-    <div className="p-16">
-      <div className="px-4">
-        <h1 className={ccss.h1}>타이틀 </h1>
-        <p className={ccss.blogDate}>- 2023. 01. 01</p>
-        <p className={ccss.blogDate}>( 2023. 12. 31 Updated )</p>
-        <div className="flex mt-4">
-          <p className={ccss.headerBtn}>카테고리</p>
-        </div>
-      </div>
-      <div className="mt-8">
-        <Image className="object-cover w-full max-h-96" width={1000} height={1000} src={imgURL + "1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF"} alt="sample image" />
-      </div>
-      {/* **** 실패 : 컴포넌트 + dangerouslySetInnerHTML 사용 테스트 */}
-      {/* prettier-ignore */}
-      <div className="my-5 leading-7">
-        <H t="Sub Title"/>
-        <p>footnote 1 <FNR c="1" /></p>
-        <p>footnote 2 <FNR c="2" /></p>
-        <p>footnote 3 <FNR c="3" /></p>
-        <p>This normal is text.</p>
-        <p><B t="This is bold text. " /></p>
-        <p><I t="This is italic text. " /></p>
-        <p><X t="This is strikethrough. " /></p>
-        <p><M t="This is marked text. " /></p>
-        <p><U t="This is underline text. " /></p>
-        <T t="Tooltip " d="This is tooltip text." />
-        <p><LT t="Text link" u="/test" /></p>
-        <LI u="/test" a="sample" i={imageIDs[0]} />
-        <BQ t="This is blockquotes <br/>blockquotes line 2" />
-        <IMG a="sample" i={imageIDs[1]} />
-        This is inline code <CI c={inlineCode} />
-        <CP c={`const a = "This is indented code.";\nconsole.log(a);`} />
-        <p>when just use <br />br tag, text height</p>
-        <p>한글을 사용할 때의 <br />자간 및 행간 테스트.</p>
-        <p>用漢字的時候<br />字體與文字間距標本</p>
-        <hr />
-        <FN n={footnotes} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="p-16">
+//       <div className="px-4">
+//         <h1 className={ccss.h1}>타이틀 </h1>
+//         <p className={ccss.blogDate}>- 2023. 01. 01</p>
+//         <p className={ccss.blogDate}>( 2023. 12. 31 Updated )</p>
+//         <div className="flex mt-4">
+//           <p className={ccss.headerBtn}>카테고리</p>
+//         </div>
+//       </div>
+//       <div className="mt-8">
+//         <Image className="object-cover w-full max-h-96" width={1000} height={1000} src={imgURL + "1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF"} alt="sample image" />
+//       </div>
+//       {/* **** 실패 : 컴포넌트 + dangerouslySetInnerHTML 사용 테스트 */}
+//       {/* prettier-ignore */}
+//       <div className="my-5 leading-7">
+//         <H t="Sub Title"/>
+//         <p>footnote 1 <FNR c="1" /></p>
+//         <p>footnote 2 <FNR c="2" /></p>
+//         <p>footnote 3 <FNR c="3" /></p>
+//         <p>This normal is text.</p>
+//         <p><B t="This is bold text. " /></p>
+//         <p><I t="This is italic text. " /></p>
+//         <p><X t="This is strikethrough. " /></p>
+//         <p><M t="This is marked text. " /></p>
+//         <p><U t="This is underline text. " /></p>
+//         <T t="Tooltip " d="This is tooltip text." />
+//         <p><LT t="Text link" u="/test" /></p>
+//         <LI u="/test" a="sample" i={imageIDs[0]} />
+//         <BQ t="This is blockquotes <br/>blockquotes line 2" />
+//         <IMG a="sample" i={imageIDs[1]} />
+//         This is inline code <CI c={inlineCode} />
+//         <CP c={`const a = "This is indented code.";\nconsole.log(a);`} />
+//         <p>when just use <br />br tag, text height</p>
+//         <p>한글을 사용할 때의 <br />자간 및 행간 테스트.</p>
+//         <p>用漢字的時候<br />字體與文字間距標本</p>
+//         <hr />
+//         <FN n={footnotes} />
+//       </div>
+//     </div>
+//   );
+// }
--- src/app/(route)/test/2/page.js
+++ src/app/(route)/test/2/page.js
@@ -1,51 +1,51 @@
-import { imgURL } from "@controller/urls";
-import * as ccss from "@controller/cssName";
-import Image from "next/image";
-import fs from "fs";
-import Markdown from "markdown-to-jsx";
-import PrismLoader from "@components/PrismLoader";
+// import { imgURL } from "@controller/urls";
+// import * as ccss from "@controller/cssName";
+// import Image from "next/image";
+// import fs from "fs";
+// import Markdown from "markdown-to-jsx";
+// import PrismLoader from "@components/PrismLoader";
 
-export default async function Test() {
-  const file = "./src/app/(route)/test/2/mdTest2.md";
-  const text = fs.readFileSync(file, "utf8");
-  // const test = await (await fetch("https://drive.google.com/uc?id=1uawh6Ixk6729fNOoOOdcn0i5RwcULL29")).text();
-  const images = ["1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF", "16TOHBZwX30pmS993qvRbZt8uUXEAR_mm"];
-  // const res = await fetch(`https://drive.google.com/uc?id=${images[0]}`);
-  // const data = res.arrayBuffer();
-  // const blob = new Blob([data], { type: res.headers.get("content-type") });
-  // const imageUrl = URL.createObjectURL(blob);
-  // const blog = await res.blob();
-  // const url = URL.createObjectURL(blog);
+// export default async function Test() {
+//   const file = "./src/app/(route)/test/2/mdTest2.md";
+//   const text = fs.readFileSync(file, "utf8");
+//   // const test = await (await fetch("https://drive.google.com/uc?id=1uawh6Ixk6729fNOoOOdcn0i5RwcULL29")).text();
+//   const images = ["1ssG03ftjdPTSJy8Co49mZ73YAOsFGntF", "16TOHBZwX30pmS993qvRbZt8uUXEAR_mm"];
+//   // const res = await fetch(`https://drive.google.com/uc?id=${images[0]}`);
+//   // const data = res.arrayBuffer();
+//   // const blob = new Blob([data], { type: res.headers.get("content-type") });
+//   // const imageUrl = URL.createObjectURL(blob);
+//   // const blog = await res.blob();
+//   // const url = URL.createObjectURL(blog);
 
-  const options = {
-    overrides: {
-      img: ({ src, ...props }) => <Image width={500} height={500} src={src} {...props} />,
-    },
-  };
-  const imglink = `https://drive.google.com/uc?id=${images[0]}`;
+//   const options = {
+//     overrides: {
+//       img: ({ src, ...props }) => <Image width={500} height={500} src={src} {...props} />,
+//     },
+//   };
+//   const imglink = `https://drive.google.com/uc?id=${images[0]}`;
 
-  const content = text.replaceAll("MDIMG=1", imglink);
-  return (
-    <div className="p-16">
-      <div className="px-4">
-        <h1 className={ccss.h1}>타이틀 </h1>
-        <p className={ccss.blogDate}>- 2023. 01. 01</p>
-        <p className={ccss.blogDate}>( 2023. 12. 31 Updated )</p>
-        <div className="flex mt-4">
-          <p className={ccss.headerBtn}>카테고리</p>
-        </div>
-      </div>
-      <div className="mt-8">
-        <Image className="object-cover w-full max-h-96" width={1000} height={1000} src={imgURL + images[0]} alt="sample image" />
-      </div>
+//   const content = text.replaceAll("MDIMG=1", imglink);
+//   return (
+//     <div className="p-16">
+//       <div className="px-4">
+//         <h1 className={ccss.h1}>타이틀 </h1>
+//         <p className={ccss.blogDate}>- 2023. 01. 01</p>
+//         <p className={ccss.blogDate}>( 2023. 12. 31 Updated )</p>
+//         <div className="flex mt-4">
+//           <p className={ccss.headerBtn}>카테고리</p>
+//         </div>
+//       </div>
+//       <div className="mt-8">
+//         <Image className="object-cover w-full max-h-96" width={1000} height={1000} src={imgURL + images[0]} alt="sample image" />
+//       </div>
 
-      <article className="prose prose-stone mt-8">
-        <PrismLoader />
-        <Markdown options={options}>{content}</Markdown>
-      </article>
-      <iframe src="https://drive.google.com/file/d/16TOHBZwX30pmS993qvRbZt8uUXEAR_mm/preview" width="640" height="480" allow="autoplay"></iframe>
-      <img width={500} height={500} src={`https://drive.google.com/uc?id=${images[0]}`} alt="Google Drive Image" />
-      <Image width={500} height={500} src={`https://drive.google.com/uc?export=view&id=${images[0]}`} alt="Google Drive Image" />
-    </div>
-  );
-}
+//       <article className="prose prose-stone mt-8">
+//         <PrismLoader />
+//         <Markdown options={options}>{content}</Markdown>
+//       </article>
+//       <iframe src="https://drive.google.com/file/d/16TOHBZwX30pmS993qvRbZt8uUXEAR_mm/preview" width="640" height="480" allow="autoplay"></iframe>
+//       <img width={500} height={500} src={`https://drive.google.com/uc?id=${images[0]}`} alt="Google Drive Image" />
+//       <Image width={500} height={500} src={`https://drive.google.com/uc?export=view&id=${images[0]}`} alt="Google Drive Image" />
+//     </div>
+//   );
+// }
