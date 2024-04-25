066fcdf003681b30d22319104532a9c9992aa41b
Dogkaebi5 <st9dog@gmail.com>
2024-04-25 12:34:27 +0900

    nav side menu 링크 무반응 처리-이벤트 디폴트 삭제

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -23,7 +23,6 @@ const Navigation = () => {
 
   // btn handle. 아이콘 변경, 메뉴 출연, 스크롤 방지
   function clickHandle(e) {
-    e.preventDefault();
     if (isActive) {
       setNavBurger("nav-burger active");
       setNavSide("nav-side active");
