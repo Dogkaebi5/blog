81b222bbd095366345416240dec331278b76f5a5
Dogkaebi5 <st9dog@gmail.com>
2024-05-24 17:32:37 +0900

    ideapad 틀 작성중

--- src/app/(route)/ideapad/page.js
+++ src/app/(route)/ideapad/page.js
@@ -11,6 +11,32 @@ export default function Product() {
       <h2 className="font-black mb-2 ">기본 기획</h2>
       <h3>서비스/상품 이름</h3>
       <input className={ccss.input + " pl-2 w-full"} />
+      <h3>문제점</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>이유</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>해결</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>분류</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>규모</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>관계</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>법</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>지원금</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>정의 누구</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>정의 언제</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>정의 무엇</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>정의 어떻게</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
+      <h3>제공</h3>
+      <input className={ccss.input + " pl-2 w-full"} />
     </div>
   );
 }
