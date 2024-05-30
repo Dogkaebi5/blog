5f791e60a6119500d6ab9ed0a3003882bf05ccee
Dogkaebi5 <st9dog@gmail.com>
2024-05-30 14:21:35 +0900

    home: search추가. 디자인 수정

--- src/app/(route)/page.js
+++ src/app/(route)/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import Heros from "@components/Heros";
+import SearchInput from "../components/SearchInput";
 
 export default function Home() {
   const cardClass = "block p-6 m-4 mx-10 rounded-xl shadow text-center font-bold text-lg hover:scale-105 hover:ring-2 ring-green-200";
   return (
     <>
       <Heros path={""} />
+      <div className="flex justify-center mt-4 mb-8">
+        <SearchInput />
+      </div>
+      <hr className="mb-6 mx-10" />
       <Link className={cardClass + " bg-green-50"} href="/cantonese">
         광둥어
       </Link>
